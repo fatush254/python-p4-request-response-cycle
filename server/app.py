337fa1237a0a896b24b0777b85eeb598855a11f8
fatush254 <fatuma.galgalo@student.moringaschool.com>
2024-07-02 12:39:01 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -1,8 +1,20 @@
-#!/usr/bin/env python3
+# server/app.py
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    host = request.headers.get('Host')
+    return f'<h1>The host for this page is {host}</h1>'
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+# example only- no need to write this out
+
+from app import app
+from flask import request
+
+request_context = app.test_request_context()
+request_context.push()
